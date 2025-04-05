f4c15b5b9dd90916fea88cd37cef4153fe271a8f
JoonIk Seo <joon980923@naver.com>
2025-04-05 20:24:10 +0900

    refactor

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,26 +1,8 @@
-// import Header from "./Header";
-// import Footer from "./Footer";
-
-// type LayoutProps = {
-//   children: React.ReactNode;
-// };
-
-// export default function Layout({ children }: LayoutProps) {
-//   return (
-//     <div className="flex min-h-screen flex-col">
-//       <Header />
-//       <main className="flex-1">{children}</main>
-//       <Footer />
-//     </div>
-//   );
-// }
-
-
 import Header from "./Header";
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 
-const Layout = () => { 
+const Layout = () => {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
@@ -30,6 +12,5 @@ const Layout = () => {
       <Footer />
     </div>
   );
-}
+};
 export default Layout;
-
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,9 +1,9 @@
-const NotFoundPage = () => {    
-    return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold">404 - Not Found</h1>
-            <p className="mt-4 text-lg">현재 페이지를 찾을 수 없습니다...</p>
-        </div>
-    );
-}
+const NotFoundPage = () => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">404 - Not Found</h1>
+      <p className="mt-4 text-lg">현재 페이지를 찾을 수 없습니다...</p>
+    </div>
+  );
+};
 export default NotFoundPage;
