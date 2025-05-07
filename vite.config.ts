290fe8943dffeeda0b1946b6740c87fb3730faa7
jam-jang <jamesjang0622@naver.com>
2025-05-07 12:50:27 +0900

    kakaologin api success

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,25 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      // 클라이언트에서 /callback 호출 시 로컬 백엔드(8080)로 전달
+      // 카카오 콜백용
       "/callback": {
-        target: "http://localhost:8080",
+        //target: "http://localhost:8080",
+         //target: "https://www.dug-out.store",
+         //target: "https://api.dug-out.store",
+         target: "https://dev.dug-out.store",
+        changeOrigin: true,
+        secure: false,
+      },
+      // /api 로 시작하는 모든 요청을 로컬 백엔드로 프록시
+      "/api": {
+        //target: "http://localhost:8080",
+         //target: "https://www.dug-out.store",
+         //target: "https://api.dug-out.store",
+        target: "https://dev.dug-out.store",
         changeOrigin: true,
         secure: false,
       },
