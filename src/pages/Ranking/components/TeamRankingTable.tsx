09ab7395ecc4c7ecb76aac2b13792bdd2d65c24a
JoonIk Seo <joon980923@naver.com>
2025-06-07 02:05:04 +0900

    주석처리

--- src/pages/Ranking/components/TeamRankingTable.tsx
+++ src/pages/Ranking/components/TeamRankingTable.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { rankingApi } from "../../../api/ranking/apis";
 // import {
 //   getEnglishTeamName,
